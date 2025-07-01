005e12cbc36fa0a0c885eb1fb50c3d0e064a71a9
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-07-01 10:26:54 -0500

    Update welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -2,11 +2,18 @@ window.addEventListener('load', () => {
   const welcomeScreen = document.getElementById('welcome-screen');
   const mainContent = document.getElementById('main-content');
 
-  setTimeout(() => {
-    welcomeScreen.classList.add('hide');
+  if (!sessionStorage.getItem('welcomeShown')) {
+    sessionStorage.setItem('welcomeShown', 'true');
+
     setTimeout(() => {
-      welcomeScreen.style.display = 'none';
-      mainContent.style.display = 'block';
-    }, 500);
-  }, 2000);
+      welcomeScreen.classList.add('hide');
+      setTimeout(() => {
+        welcomeScreen.style.display = 'none';
+        mainContent.style.display = 'block';
+      }, 500);
+    }, 2000);
+  } else {
+    welcomeScreen.style.display = 'none';
+    mainContent.style.display = 'block';
+  }
 });
